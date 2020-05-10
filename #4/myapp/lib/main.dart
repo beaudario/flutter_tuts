148629c899528e5fe11c609009a9d19f3aa54b6e
beaudario <dario.douma@upcmail.nl>
2020-05-10 17:40:59 +0200

    lesson 6 done

--- #4/myapp/lib/main.dart
+++ #4/myapp/lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hello World'),
+      child: Text(
+        'Hello World',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text('click'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
