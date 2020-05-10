618960c929c46ad02cd22592110fa1e796ba01cd
beaudario <dario.douma@upcmail.nl>
2020-05-10 18:31:55 +0200

    lesson 8 done

--- #4/myapp/lib/main.dart
+++ #4/myapp/lib/main.dart
@@ -14,16 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: Image.asset('assets/space-1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
